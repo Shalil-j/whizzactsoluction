eca2328dbc4a54cfaa77c94c8f9b5223d79ec589
shalil-j <shaliljaiswar462@gmail.com>
2022-08-13 16:42:56 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+AOS.init();
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(window).height()){
+            $(".navbar").css({"background-color":"white"});   
+        }
+        else{
+            $(".navbar").css({"background-color":"#212529"});
+        }
+
+    })
+})
