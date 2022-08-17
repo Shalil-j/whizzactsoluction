813f1e96e1082ad1e2e4d6ac75c82ea2c67ac618
shalil-j <shaliljaiswar462@gmail.com>
2022-08-18 01:53:42 +0530

    1

--- js/index.js
+++ js/index.js
@@ -2,11 +2,17 @@ AOS.init();
 
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height()){
-            $(".navbar").css({"background-color":"white"});   
+        var scroll = $(window).scrollTop();
+        if(scroll > 10){
+            $(".navbar").css({"background-color":"white"});
+            $(".links").css({"color":"black"});    
+            $(".service-link").css({"color":"black"}); 
         }
         else{
-            $(".navbar").css({"background-color":"#212529"});
+            $(".navbar").css({"background-color":"transparent"});
+            $(".links").css({"color":"white"});  
+            $(".service-link").css({"color":"white"}); 
+
         }
 
     })
