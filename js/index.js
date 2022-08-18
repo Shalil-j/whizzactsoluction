dbff8f7a1ffa65e109b111abcb90195dc0306c83
shalil-j <shaliljaiswar462@gmail.com>
2022-08-19 01:41:44 +0530

    1

--- js/index.js
+++ js/index.js
@@ -4,14 +4,24 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if(scroll > 10){
-            $(".navbar").css({"background-color":"white"});
-            $(".links").css({"color":"black"});    
-            $(".service-link").css({"color":"black"}); 
+            $(".navbar-expand-lg").css({"background-color":"white"});
+            $(".navbar-expand-lg").addClass('shadow');
+            $(".navbar-expand-lg").removeClass('shadow-0');
+            $(".service-link").addClass('text-black'); 
+            $(".service-link").removeClass('text-white'); 
+            $(".links").addClass('text-black'); 
+            $(".links").removeClass('text-white');    
+
+
         }
         else{
-            $(".navbar").css({"background-color":"transparent"});
-            $(".links").css({"color":"white"});  
-            $(".service-link").css({"color":"white"}); 
+            $(".navbar-expand-lg").addClass('shadow-0');
+            $(".navbar-expand-lg").removeClass('shadow');
+            $(".navbar-expand-lg").css({"background-color":"transparent"});
+            $(".service-link").addClass('text-white'); 
+            $(".service-link").removeClass('text-black');  
+            $(".links").addClass('text-white'); 
+            $(".links").removeClass('text-black'); 
 
         }
 
---
+++ js/industry.js
@@ -0,0 +1,20 @@
+$("#medical").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".medical-div").offset().top},
+        'slow');
+});
+$("#manif").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".manifacturing-div").offset().top},
+        'slow');
+});
+$("#education").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".education-div").offset().top},
+        'slow');
+});
+$("#retailer").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".retailer-div").offset().top},
+        'slow');
+});
